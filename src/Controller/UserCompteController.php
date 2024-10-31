b4fda487108bac15e8e193c56f2797347e7eb397
melaa <melaa976@gmail.com>
2024-10-31 14:30:11 +0100

    configuration page utilisateur

--- src/Controller/UserCompteController.php
+++ src/Controller/UserCompteController.php
@@ -6,14 +6,44 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class UserCompteController extends AbstractController
+
+#[Route('/user/compte')]
+final class UserCompteController extends AbstractController
 {
-    #[Route('/user/compte', name: 'app_user_compte')]
+    #[Route(name: 'app_user_compte')]
     public function index(): Response
     {
+        $user = $this->getUser();
         
-        return $this->render('user_compte/index.html.twig', [
-            'controller_name' => 'UserCompteController',
+        //info sur l'utilisateur
+    
+        return $this->render('user_compte/indexUser.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+
+    #[Route('/infouser', name: 'app_user_compte_info')]
+    public function infUser(): Response
+    {
+        $user = $this->getUser();
+        
+        //info sur l'utilisateur
+    
+        return $this->render('user_compte/infouser.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/offrepst', name: 'app_user_compte_offrepst')]
+    public function infUserOffre(): Response
+    {
+        $user = $this->getUser();
+        
+        //info sur l'utilisateur
+    
+        return $this->render('user_compte/offrepst.html.twig', [
+            'user' => $user,
         ]);
     }
 }
