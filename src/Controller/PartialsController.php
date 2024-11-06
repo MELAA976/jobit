022708ea135be0d465b255b4dc3ca9b80bfbfe98
melaa <melaa976@gmail.com>
2024-11-06 16:44:27 +0100

    add sass bundle

---
+++ src/Controller/PartialsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+#[Route('/partials')]
+class PartialsController extends AbstractController
+{
+    #[Route(name: 'app_partials_header')]
+    public function header(): Response
+    {
+        return $this->render('partials/header.html.twig', [
+            'controller_name' => 'PartialsController',
+        ]);
+    }
+
+
+    #[Route(name: 'app_partials_header')]
+    public function  footer(): Response
+    {
+        return $this->render('partials/header.html.twig', [
+            'controller_name' => 'PartialsController',
+        ]);
+    }
+
+}
